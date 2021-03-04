ea79875ba058bc1e5c445bb04c35fe61be8a4e9a
donglu <donglu@starot.com>
2021-03-04 10:29:44 +0800

    merge 2019.4.19

---
+++ Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using Conditional = System.Diagnostics.ConditionalAttribute;
+
+public class CustomRenderPipeline : RenderPipeline
+{
+    CullingResults cull;
+    Material errorMaterial;
+    CommandBuffer cameraBuffer = new CommandBuffer() {
+        name = "Render Camera"
+    };
+
+    CameraRenderer renderer = new CameraRenderer();
+
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras) {
+        foreach(var camera in cameras) {
+            renderer.Render(context, camera);
+        }
+    }
+}
